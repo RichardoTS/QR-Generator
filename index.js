481f60863225b46412d10eab6bf8fada8fa7575b
RichardoTS <ri.tapiasaez@gmail.com>
2023-07-18 02:44:08 +0000

    Input and QR working

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 import inquirer from "inquirer";
 import qr from "qr-image";
+import fs, { writeFile } from "fs"
 
 inquirer
     .prompt([
         { message: "Type in your favorite URL: ", name: "URL" }
     ])
     .then((answers) => {
-        console.log(answers)
+        const url = answers.URL;
+
+        var qr_svg = qr.image(url);
+        qr_svg.pipe(fs.createWriteStream('qr-image.png'));
+
+        fs.writeFile('URL.txt', url, (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+          });
     })
     .catch((error) => {
         if (error.isTtyError) {
